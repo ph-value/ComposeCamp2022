27ce4bf30b05b64142cdaea85b11b97fb96a9f43
ph-value <phvalue123@gmail.com>
2022-11-10 21:06:40 +0900

    [pathway1] jetpack Compose Basic 8. 상태 호이스팅

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,27 +23,59 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// 호이스팅이란 들어 올린다, 끌어 올린다 라는 의미.
+// 상태를 호이스팅할 수 있게 하면 상태가 중복되지 않고 버그가 발생하는 것을 방지, 컴포저블 재사용, 쉽게 테스트 가능.
+// 호이스팅 하면 안될 때 : 컴포저블의 상위 요소에서 제어할 필요가 없는 상태
 @Composable
-private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    // TODO: 이 상태는 호이스팅 되어야 한다!
+
     Surface(color = MaterialTheme.colors.background) {
         Column(
-            modifier = Modifier
-                .fillMaxWidth(4f)
-                .padding(vertical = 4.dp)
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            for (name in names) {
-                Greeting(name = name)
+            Text(text = "Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text(text = "Continue")
             }
         }
+
     }
 }
 
 @Composable
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        // 상태가 아닌 함수를 전달하는 방식으로 컴포저블의 재사용 가능성을 높이고
+        // 다른 컴포저블이 상태를 변경하지 않도록 보호한다.
+
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Surface(color = MaterialTheme.colors.background) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(4f)
+                    .padding(vertical = 4.dp)
+            ) {
+                for (name in names) {
+                    Greeting(name = name)
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
 fun Greeting(name: String) {
-    /*
-        변수에 remember 없이 mutavleStateOf 할당 하면 컴포저블을 다시 호출 할 때 언제든지 리컴포지션이 일어날 수 있다.
-        여러 리컴포지션 간에 상태를 유지하려고 remember를 사용해 변경 가능한 상태를 기억해야한다.
-    */
     val expanded = remember { mutableStateOf(false) }
     val extraPaddingValues = if (expanded.value) 48.dp else 0.dp
     Surface(
@@ -57,9 +87,11 @@ fun Greeting(name: String) {
                 .padding(24.dp)
                 .fillMaxWidth()
         ) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPaddingValues)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPaddingValues)
+            ) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
@@ -76,5 +108,13 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     BasicsCodelabTheme {
         MyApp()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {}) // Do nothing on click.
     }
 }
