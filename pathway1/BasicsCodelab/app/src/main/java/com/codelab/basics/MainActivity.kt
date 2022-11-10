c772e974b6d4f4f6706d7270e837ef44487d7bc7
ph-value <phvalue123@gmail.com>
2022-11-10 20:29:23 +0900

    [pathway1] jetpack Compose Basic 6. 열과 행 만들기(솔루션)

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -3,14 +3,10 @@ package com.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,9 +26,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun MyApp(names: List<String> = listOf("World", "Compose")) {
     Surface(color = MaterialTheme.colors.background) {
-        Column(modifier = Modifier
-            .fillMaxWidth(4f)
-            .padding(vertical = 4.dp)) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(4f)
+                .padding(vertical = 4.dp)
+        ) {
             for (name in names) {
                 Greeting(name = name)
             }
@@ -46,12 +44,20 @@ fun Greeting(name: String) {
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Column(modifier = Modifier
-            .padding(24.dp)
-            .fillMaxWidth()) {
-            Text(text = "Hello, ")
-            Text(text = name)
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+                .fillMaxWidth()
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text(text = "Show more")
+            }
         }
+
     }
 }
 
